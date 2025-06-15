b65fffdee1626ea3710a129366cb0d376860f296
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-06-15 18:47:59 -0400

    feat: add providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,6 @@
+import { AuthProvider } from "@/contexts/auth/AuthContext";
+
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return <AuthProvider>{children}</AuthProvider>;
+}
