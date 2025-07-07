31aa754af19441bc557edeb4f27bfc14a5f4e1d4
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2025-07-06 23:06:27 -0400

    feat: add Cart page and enhance Navbar with user profile and cart functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import { useAuth } from '@/hooks/useAuth';
+import { useCartStore } from '@/stores/CartStore';
+import { ShoppingCartIcon, User, UserIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export const Navbar = () => {
+
+  const {items: cart} = useCartStore();
+  const {user} = useAuth();
+
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <nav className="bg-white shadow-md w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
-          <a href="/">
+          <Link href="/">
             <span className="text-xl font-bold text-gray-800 hover:text-black cursor-pointer">
               MiTienda
             </span>
-          </a>
+          </Link>
 
           {/* Enlaces */}
           <div className="hidden md:flex space-x-6">
@@ -27,7 +36,15 @@ export const Navbar = () => {
             </a>
           </div>
 
-          {/* Botón Login */}
+          <div className="flex items-center space-x-4">
+            {user ? (
+            <div>
+              <Link href={'/profile'} className="flex items-center hover:bg-blue-400 rounded-full p-2 transition">
+                <UserIcon className="h-5 w-5 mr-2" />
+                <span className="ml-2">{user.firtsName}</span>
+              </Link>
+            </div>
+          ):
           <div>
             <Link href="/login">
               <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition">
@@ -35,6 +52,22 @@ export const Navbar = () => {
               </button>
             </Link>
           </div>
+          }
+
+          <div>
+            <Link href={'/cart'} className="relative flex item-center hover:bg-blue-400 rounded-full p-2 transition-all">
+              <ShoppingCartIcon className="w-6 h-6"/>
+              {
+                totalItems > 0 && (
+                  <span className="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold rounded-full px-2">
+                    {totalItems}
+                  </span>
+                )
+              }
+            </Link>
+          </div>
+          </div>
+
         </div>
       </div>
     </nav>
